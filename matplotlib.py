eec7c8d604f6b72cc9230164ab86bf893dd846b1
adeku <Adekualameem@gmail.com>
2024-10-28 15:39:53 -0500

    10/28/24

--- matplotlib.py
+++ matplotlib.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
-length = [10,20,30,40,50]
-width = [1,2,3,4,5]
-plt.plot(length,width,ls=' ',marker='o')
+import pandas as pd
+length = pd.Series([10,20,30,40,50])
+width = pd.Series([1,2,3,4,5])
+df = pd.concat([length,width],axis=1)
+print(df)
--- panda.py
+++ panda.py
@@ -12,13 +12,22 @@ data = pd.read_csv('data.csv')
 
 age_12 = pd.Series([None,2.0,4.0,2.0,4.0,2.0])
 age_teen = pd.Series([None,7.0,None,14.0,None,7.0])
-pclass = pd.Series([3,2,3,3,2,3])
+pclass = pd.Series([4,2,3,3,2,3])
 pas = pd.DataFrame()
 pas = pd.concat([age_12,age_teen,pclass],axis=1)
 pas.columns = ['Age_Teen','Age_12','Pclass']
 pas.index = [1,3,5,0,10,22]
-# print(pas.iloc[:3,:3])
+# pas.dropna(subset=['Age_Teen'],inplace=True)
+# print(pas)
 # iloc enables access with index number regardless of label
 # print(pas.groupby(['Age_12']).Pclass.median())
 
 # print(data.iloc[0].mode())
+
+df = pd.DataFrame({
+  'Name': ['Alice', 'Bob', 'Charlie', 'Alice', 'Bob'],
+  'City': ['New York', 'London', 'New York', 'London', 'Tokyo'],
+  'Sales': [100, 200, 150, 120, 250]
+})
+grouped = df.groupby('City')
+print(grouped.get_group('Sales'))
